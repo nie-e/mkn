e3b658ad84276f61934cf8590a699744334d891b
Jason Fung <solidzoro@live.com>
2020-11-25 10:45:56 +0800

    refactor: minify pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
-import Home from './home';
+import { Button } from 'antd';
+
+import styles from './styles.module.less';
+// import './styles.less';
+require('./styles.less');
 
 export default function Index() {
-  return <Home />;
+  return (
+    <div className={styles['page-index-wrapper']}>
+      <div className={styles['page-index-inner']}>
+        <div className={styles['title']}>Next.js CSS (module) + less + Antd</div>
+
+        <Button type="primary" ghost size="large">
+          Antd Button
+        </Button>
+        <br />
+        <br />
+        <button className="gbutton">CSS Button</button>
+        <br />
+        <br />
+        <button className={styles['m-button']}>CSS Module Bodule</button>
+      </div>
+    </div>
+  );
 }
